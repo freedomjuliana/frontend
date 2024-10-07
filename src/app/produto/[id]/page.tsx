a60eed5417f7f6f33e3cc232eca05cc5b8e4b2ce
Juliana Morais <jujubratz27@gmail.com>
2024-10-06 22:12:57 -0300

    frontend

---
+++ src/app/produto/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { produtos } from "@/core";
+
+export default function PaginaProduto(props: any) {
+  const id = +props.params.id;
+  const produto = produtos.find((p) => p.id === id);
+  return produto ? (
+    <div>{produto?.nome}</div>
+  ) : (
+    <div>Produto não encontrado</div>
+  );
+}
